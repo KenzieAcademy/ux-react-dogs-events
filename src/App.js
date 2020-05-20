70253835d0438a9e472aad376d6fc90a7dc47b3c
PUT YOUR NAME HEREdwrightdesigns <denise@denisewrightdesigns.com>
2020-05-20 12:30:47 -0500

    Handling Events Practice
    Practice Completed

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ class App extends Component {
   state = {
     dogs: [
       {
-        name: "Scruffles",
+        name: "Mon-El",
       },
       {
-        name: "Snarf",
+        name: "Kara",
       },
       {
-        name: "Wookie",
+        name: "Mischa",
       },
     ],
+    newDogName: "",
+  };
+
+  handleChangeDogName = (event) => {
+    this.setState({ newDogName: event.target.value });
+  };
+  handleAddNewDog = () => {
+    let newDog = {
+      name: this.state.newDogName,
+    };
+    this.setState((state) => ({
+      dogs: [...state.dogs, newDog],
+      newDogName: "",
+    }));
   };
 
   render() {
@@ -24,14 +38,22 @@ class App extends Component {
         <Header numDogs={this.state.dogs.length} />
         <h2>My Dogs</h2>
         <ul className="dogList">
-          {this.state.dogs.map((dog) => (
-            <li>
+          {this.state.dogs.map((dog, index) => (
+            <li key={index}>
               <Dog dog={dog} />
             </li>
           ))}
         </ul>
         <br />
-        <div>Add Input and button here!</div>
+        <div>
+          <input className ="newDog"
+            type="text"
+            placeholder="Input Dog Name Here"
+            value={this.state.newDogName}
+            onChange={this.handleChangeDogName}
+          ></input>
+          <button className="newDog-btn" onClick={this.handleAddNewDog} disabled={this.state.newDogName === ""}>Add New Dog</button>
+        </div>
       </div>
     );
   }
--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,8 +1,18 @@
 // Dog.js
 import React from "react";
 
+const petDog = (dogName) => {
+  alert(`You pet ${dogName}`)
+} 
+
 function Dog(props) {
-  return <div>{props.dog.name}</div>;
+
+  return (
+    <div>
+      {props.dog.name}
+      <button className="pet-btn" onClick={() => petDog(props.dog.name)}>Pet {props.dog.name}</button>
+    </div>
+  );
 }
 
 export default Dog;
