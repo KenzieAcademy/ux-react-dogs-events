c710c2cc93e80ddf100f03300f0dfd916c01e2d2
benfindley18 <benfindley18.com>
2020-05-20 13:42:23 -0400

    finished practice

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,8 +1,20 @@
 // Dog.js
 import React from "react";
 
+
+
 function Dog(props) {
-  return <div>{props.dog.name}</div>;
+  const handlePetDog = (event) => {
+    alert(`You pet ${props.dog.name}`)
+  }
+
+  return (
+    <div>
+      <p>{props.dog.name}</p>
+      <button onClick={handlePetDog}> Pet {props.dog.name}</button>
+    </div>
+  )
+
 }
 
 export default Dog;
