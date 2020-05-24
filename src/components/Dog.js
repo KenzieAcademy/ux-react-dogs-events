dc41df4f5cf5a12ce7011dbc8e9a80ef3dff03ae
jeann9ne <jeanninecoons@gmail.com>
2020-05-24 18:38:38 -0400

    complete

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,8 +1,13 @@
 // Dog.js
 import React from "react";
 
+const petTheDog = (dog) =>{
+  alert(`You pet ${dog}!`)
+};
+
 function Dog(props) {
-  return <div>{props.dog.name}</div>;
-}
+  return <div>{props.dog.name}
+  <button onclick={() => petTheDog(props.dog.name)}>Pet the dog!</button></div>
+  ;}
 
 export default Dog;
